45126ee4641b9feb5a7a9b66e90c5fcffc7bc9a7
tstaffas <37420485+tstaffas@users.noreply.github.com>
2018-04-19 20:03:26 +0200

    new maps

---
+++ uppg1/linked_lists.java
@@ -0,0 +1,34 @@
+public class linked_lists {
+
+    public class list_element<T>{
+
+        private T element;
+        private T pointer;
+    }
+
+    public class linked_lists<T>{
+
+        public T first_elemnt;
+        public T last_element;
+        public int number_of_elements = 0;
+
+        public void add_first_element(T){ }
+
+        public void add_last_element(T){ }
+
+        public get get(index){ }
+
+        public remove_first{ }
+
+        private clear {}
+
+        public int get_seize(){
+            return number_of_elements;
+        }
+    }
+
+    public static void main(String[] args){
+
+
+    }
+}
